48c95290bfdef2fbd30667667f54a63baeb02bee
Tom <tomacunningham@gmail.com>
2023-03-21 21:14:41 -0500

    Adding in microractor test2

---
+++ MicroTractorTest2/MicroTractorTest2.ino
@@ -0,0 +1,108 @@
+/*************************************************
+//
+//  MicroTractor v2.0
+//
+//  Cunningham 03/09/2023
+//
+**************************************************/
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
+#include <Servo.h>
+
+#define PIN           D3
+#define SDA           D2
+#define SCL           D1
+
+#define MOTOR_ENABLE  D4
+
+#define MOTOR_A_POS   D5
+#define MOTOR_A_NEG   D6
+#define MOTOR_A_PWM   8
+
+#define MOTOR_B_POS   D7
+#define MOTOR_B_NEG   D8
+#define MOTOR_B_PWM   9
+
+#define SERVO_1       0
+#define SERVO_2       1
+#define SERVO_3       2
+#define SERVO_4       3
+#define SERVO_5       4
+#define SERVO_6       5
+#define SERVO_7       6
+#define SERVO_8       7
+
+
+#define MIN_PULSE_WIDTH       450
+#define MAX_PULSE_WIDTH       2450
+#define DEFAULT_PULSE_WIDTH   1500
+#define FREQUENCY             200
+
+//Configure the PWM chip for the tractor
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x52);
+
+void setup() 
+{ 
+  //Setup the i2c bus
+  Wire.begin(SDA, SCL);
+  pwm.begin();
+  pwm.setPWMFreq(FREQUENCY);
+
+  //Drive motor
+  pinMode(MOTOR_A_POS,  OUTPUT);
+  pinMode(MOTOR_A_NEG,  OUTPUT);
+  pinMode(MOTOR_ENABLE, OUTPUT);
+  pinMode(MOTOR_B_POS,  OUTPUT);
+  pinMode(MOTOR_B_NEG,  OUTPUT);
+  
+  digitalWrite(MOTOR_A_NEG, LOW);
+  digitalWrite(MOTOR_A_POS, HIGH);
+
+  digitalWrite(MOTOR_B_POS, LOW);
+  digitalWrite(MOTOR_B_NEG, HIGH);
+  
+  pwm.setPWM(MOTOR_A_PWM, 50, 0);
+  pwm.setPWM(MOTOR_B_PWM, 50, 0);
+  
+  //Turn on external LED
+  pinMode(D0, OUTPUT);
+  digitalWrite(D0, HIGH);
+  
+  //short delay to get things kicked off
+  delay(100);
+
+}
+
+void loop() 
+{
+   digitalWrite(D0, HIGH);
+   digitalWrite(MOTOR_ENABLE, HIGH);
+   pulseWidth(150,SERVO_4);
+   delay(400);
+   pulseWidth(150,SERVO_3);
+   delay(400);
+   pulseWidth(150,SERVO_2);
+   delay(400);
+   pulseWidth(150,SERVO_1);
+   delay(400);
+   pulseWidth(60,SERVO_4);
+   delay(400);
+   pulseWidth(60,SERVO_3);
+   delay(400);
+   pulseWidth(60,SERVO_2);
+   delay(400);
+   pulseWidth(60,SERVO_1);
+   digitalWrite(D0, LOW);
+   delay(400);
+}
+
+int pulseWidth(int angle, int servo_ID)
+{
+  int pulse_wide, analog_value;
+  pulse_wide   = map(angle, 0, 180, MIN_PULSE_WIDTH, MAX_PULSE_WIDTH);
+  analog_value = int(float(pulse_wide) / 1000000 * FREQUENCY * 4096);
+  pwm.setPWM(servo_ID, 0, analog_value);
+  return analog_value;
+}
